c23d0ea2667d215e4efffb1f321054df3f447278
Huntley <huntleycastner@gmail.com>
2023-03-20 18:34:08 -0700

    adding styles

--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,24 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
-  
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <div>
+          <img src="public\images\brenda-godinez-MsTOg6rhRVk-unsplash.jpg" alt="Chia Fruit Shake" />
+          <div>
+            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
+        <a href="/places">
+          <button styles="style.css" className="btn-primary">Places Page</button>
+        </a>
+      </main>
+
+    </Def>
+  )
+}
 
 module.exports = home
