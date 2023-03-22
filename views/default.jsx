64fb41c54eb7ccfb3011f61155d37af7d2bcc779
Huntley <huntleycastner@gmail.com>
2023-03-21 18:11:14 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,6 +1,6 @@
 const React = require('react')
 
-function Def (html) {
+function Def(html) {
     return (
         <html>
             <head>
@@ -9,6 +9,19 @@ function Def (html) {
                 <link rel='stylesheet' href='/css/style.css'></link>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
