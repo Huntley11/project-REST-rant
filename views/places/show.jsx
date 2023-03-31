7905063b695ada84724418e1ef77ea4d53b224ae
Huntley <huntleycastner@gmail.com>
2023-03-30 18:50:49 -0700

    Added the edit form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,22 +1,29 @@
 const React = require('react')
-const Def = require('../default')
+const Default = require('../default')
 
-function show(data) {
-  return (
-    <Def>
-      <main>
-        <h1>{data.place.name}</h1>
-      </main>
-      <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-        Edit
-      </a>
-      <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-        <button type="submit" className="btn btn-danger">
-          Delete
-        </button>
-      </form>
-    </Def>
-  )
+function Show(data) {
+    return (
+        <Default>
+          <main>
+            <h1>{data.place.name}</h1>
+            <div>
+                <h3>Ratings</h3>
+            </div>
+            <div>
+                <h3>Description</h3>
+            </div>
+            <div>
+                <h4>Comments</h4>
+            </div>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form>     
+          </main>
+        </Default>
+    )
 }
 
-module.exports = show
+module.exports = Show
